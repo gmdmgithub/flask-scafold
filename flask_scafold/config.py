a67798b7043558d472440389bc1a588c7e2242c0
gmdmgithub <gmika@interia.pl>
2019-01-11 23:27:09 +0100

    reminder - db creation

--- flask_scafold/config.py
+++ flask_scafold/config.py
@@ -6,5 +6,5 @@ class Config:
     ''' config class for environment dependencies '''
     env_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path=env_path) #this cause .env is a part of os system variables
-    SECRET_KEY = os.environ.get("SECRET_KEY") # expose to easily access in other classes
+     SECRET_KEY = os.environ.get("SECRET_KEY") # expose to easily access in other classes
     
